4440213acfb74fc455534ddd26c4464272f1f756
Łupina <lupina385@gmail.com>
2019-06-03 22:23:51 +0200

    Now results display in listbox.

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ image = Image.open("robot.jpg")
 image = image.resize((400, 400), Image.ANTIALIAS)
 img = ImageTk.PhotoImage(image)
 img_label = tk.Label(top, image=img, height=400, width=400)
-img_label.grid(column=2, row=0, rowspan=1000, sticky='n')
+img_label.grid(column=2, row=0, rowspan=17, sticky='n')
 
 # Angles
 angle =[]
@@ -50,17 +50,18 @@ sav2.append(el_block(top, 's2:', 14))
 sav2.append(el_block(top, 'V2:', 15))
 sav2.append(el_block(top, 'a2:', 16))
 
-label_result = []
-for i in range(0, 8):
-    label_result.append(tk.Label(top, text='', font = 'Arial'))
-    label_result[i].grid(column=i+3, row=0, rowspan=100, sticky='n')
-label_result.append(tk.Label(top, text='', font = 'Arial', justify='left'))
-label_result[8].grid(column=3, row=5, rowspan=10, sticky='nw')
-label_result.append(tk.Label(top, text='', font = 'Arial', justify='left'))
-label_result[9].grid(column=3, row=10, rowspan=10, sticky='nw')
+result_frame = tk.Frame(top)
+result_frame.grid(row=17, sticky='nesw', columnspan=3)
 
-a_button = tk.Button(text='Oblicz', font='arial', command=lambda: oc.calculate(angle, length, omega, epsilon, sav2, label_result))
-a_button.grid(column=10, sticky='se')
+result_listbox = tk.Listbox(result_frame, font=20, width=65)
+result_listbox.grid(sticky='nesw')
+scrollbar = tk.Scrollbar(result_frame)
+scrollbar.grid(column=1, row=0, sticky='ns')
+result_listbox.config(yscrollcommand=scrollbar.set)
+scrollbar.config(command=result_listbox.yview)
+
+a_button = tk.Button(text='Oblicz', font='arial', command=lambda: oc.calculate(angle, length, omega, epsilon, sav2, result_listbox))
+a_button.grid(column=2, sticky='se')
 
 
 top.mainloop()
--- on_click.py
+++ on_click.py
@@ -3,7 +3,7 @@ from tkinter import messagebox as tkmb
 import calculations as calc
 import numpy as np
 
-def calculate(angle, length, omega, epsilon, sav2, label):
+def calculate(angle, length, omega, epsilon, sav2, result_listbox):
     """It displays all outcomes needed in given labels"""
     np.set_printoptions(floatmode = 'maxprec_equal')
     cords = calc.positions(angle, length)
@@ -11,82 +11,80 @@ def calculate(angle, length, omega, epsilon, sav2, label):
         tkmb.showinfo('Błąd', 'Wprowadzono nieprawidłową wartość długości lub kąta!')
         return 0
     else:
-        result = 'Współrzędne członów:\nA( {} ; {} )\nB( {} ; {} )\nC( {} ; {} )\nD( {} ; {} )'.format(cords['A'][0], cords['A'][1], cords['B'][0], cords['B'][1], cords['C'][0], cords['C'][1], cords['D'][0], cords['D'][1])
-        label[0].config(text=result)
+        result_listbox.insert('end', 'Współrzędne członów:')
+        result_listbox.insert('end', 'A( {} ; {} )'.format(cords['A'][0], cords['A'][1]))
+        result_listbox.insert('end', 'B( {} ; {} )'.format(cords['B'][0], cords['B'][1]))
+        result_listbox.insert('end', 'C( {} ; {} )'.format(cords['C'][0], cords['C'][1]))
+        result_listbox.insert('end', 'D( {} ; {} )'.format(cords['D'][0], cords['D'][1]))
+
+        #label[0].config(text=result)
+
 
     v = calc.velocities(angle, length, omega, sav2)
     if v == 'error':
         tkmb.showinfo('Błąd', 'Wprowadzono nieprawidłową wartość kąta, długości bądź prędkości kątowej!')
         return 0
     else:
-        result = 'Prędkości: '
+        result_listbox.insert('end','Prędkości:')
         for i in range(1, 8):
-            result += '\n===V' + str(i) + '===\n{}'.format(v[i])
-        label[1].config(text=result)
+            result_listbox.insert('end', 'V' + str(i) + ': {}'.format(v[i]))
 
     a = calc.accelerations(angle, length, omega, epsilon, sav2)
     if a == 'error':
         tkmb.showinfo('Błąd', 'Wprowadzono nieprawidłową wartość kąta, długości, prędkości kątowej lub przyspieszenia kątowego!')
         return 0
     else:
-        result = 'Przyspieszenia:'
+        result_listbox.insert('end','Przyspieszenia:')
         for i in range(1, 8):
-            result += '\n===a' + str(i) + '===\n{}'.format(a[i])
-        label[2].config(text=result)
+            result_listbox.insert('end','a' + str(i) + ': {}'.format(a[i]))
 
     acm = calc.accelerations_centre_mass(angle, length, omega, epsilon, sav2)
     if acm == 'error':
         tkmb.showinfo('Błąd', 'Wprowadzono nieprawidłową wartość kąta, długości, prędkości kątowej lub przyspieszenia kątowego!')
     else:
-        result = 'Przysp. śr. ciężkości:'
-        result += '\n===a01===\n{}'.format(acm[0])
-        result += '\n===a23===\n{}'.format(acm[1])
-        result += '\n===a34===\n{}'.format(acm[2])
-        result += '\n===a45===\n{}'.format(acm[3])
-        result += '\n===a67===\n{}'.format(acm[4])
-        label[3].config(text=result)
+        result_listbox.insert('end','Przyspieszenia środków ciężkości:')
+        result_listbox.insert('end','a01: {}'.format(acm[0]))
+        result_listbox.insert('end','a23: {}'.format(acm[1]))
+        result_listbox.insert('end','a34: {}'.format(acm[2]))
+        result_listbox.insert('end','a45: {}'.format(acm[3]))
+        result_listbox.insert('end','a67: {}'.format(acm[4]))
 
     F = calc.fictitious_forces(length, angle, omega, epsilon, sav2)
     if F == 'error':
         tkmb.showinfo('Błąd', 'Wprowadzono nieprawidłową wartość kąta, długości, prędkości kątowej lub przyspieszenia kątowego!')
     else:
-        result = 'Siły bezwł.:'
+        result_listbox.insert('end','Siły bezwładności:')
         for i in range(0, 5):
-            result += '\n===F' + str(i) + '===\n{}'.format(np.round(F[i], 3))
-        label[4].config(text=result)
+            result_listbox.insert('end', 'F' + str(i) + ': {}'.format(np.round(F[i], 3)))
 
     N = calc.torques(length, angle, omega, epsilon, sav2)
     if N == 'error':
         tkmb.showinfo('Błąd', 'Wprowadzono nieprawidłową wartość kąta, długości, prędkości kątowej lub przyspieszenia kątowego!')
     else:
-        result = 'Momenty sił:'
+        result_listbox.insert('end','Momenty sił bezwładności:')
         for i in range(0, 5):
-            result += '\n===N' + str(i) + '===\n{}'.format(np.round(N[i], 8))
-        label[5].config(text=result)
+            result_listbox.insert('end', 'N' + str(i) + ': {}'.format(np.round(N[i], 8)))
 
     f = calc.forces_of_interactions(length, angle, omega, epsilon, sav2)
     if f == 'error':
         tkmb.showinfo('Błąd', 'Wprowadzono nieprawidłową wartość kąta, długości, prędkości kątowej lub przyspieszenia kątowego!')
     else:
-        result = 'Siły oddziaływań członów:'
+        result_listbox.insert('end', 'Siły oddziaływań członów:')
         for i in range(0, 7):
-            result += '\n===f' + str(i+1) + '===\n{}'.format(np.round(f[i], 3))
-        label[6].config(text=result)
+            result_listbox.insert('end', 'f' + str(i+1) + ': {}'.format(np.round(f[i], 3)))
+
 
     n = calc.moments_of_impact(length, angle, omega, epsilon, sav2)
-    result = 'Momenty oddziaływań członów:'
+    result_listbox.insert('end', 'Momenty oddziaływań członów:')
     for i in range(0, 7):
-        result += '\n===n' + str(i+1) + '===\n{}'.format(np.round(n[i], 3))
-    label[7].config(text=result)
+        result_listbox.insert('end', 'n' + str(i+1) + ': {}'.format(np.round(n[i], 3)))
 
     df = calc.driving_forces(length, angle, omega, epsilon, sav2)
-    result = 'Siły napędowe:'
+    result_listbox.insert('end', 'Siły napędowe:')
     for i in range(0, 7):
-        result += '\nSN' + str(i+1) + '= {}'.format(round(df[i], 3))
-    label[8].config(text=result)
+        result_listbox.insert('end', 'SN' + str(i+1) + '= {}'.format(round(df[i], 3)))
 
     dm = calc.driving_moments(length, angle, omega, epsilon, sav2)
-    result = 'Momenty napędowe:'
+    result_listbox.insert('end', 'Momenty napędowe:')
     for i in range(0, 7):
-        result += '\nMo' + str(i+1) + '= {}'.format(np.round(dm[i], 3))
-    label[9].config(text=result)
+        gitresult_listbox.insert('end', 'Mo' + str(i+1) + '= {}'.format(np.round(dm[i], 3)))
